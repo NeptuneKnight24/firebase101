185c54e7440c66fd19e250379a34eebd7178a613
John Christopher Columbres <jhegs.knightdev@gmail.com>
2017-03-21 10:26:35 +0800

    Learned how to save different type of data

--- app/src/main/java/org/knightdev/firebase101/ProfileActivity.java
+++ app/src/main/java/org/knightdev/firebase101/ProfileActivity.java
@@ -6,15 +6,25 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.api.model.StringList;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnTouchListener{
     private FirebaseAuth firebaseAuth;
     private TextView textViewUserEmail;
     private Button buttonLogout;
+
+    private DatabaseReference databaseReference;
+
+    private EditText editTextname,editTextAddress;
+    private Button buttonSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +36,33 @@ public class ProfileActivity extends AppCompatActivity implements View.OnTouchLi
             finish();
             startActivity(new Intent(this,LoginActivity.class));
         }
-
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        editTextname = (EditText)findViewById(R.id.et_name);
+        editTextAddress = (EditText)findViewById(R.id.et_address);
+        buttonSave = (Button)findViewById(R.id.btn_AddPeople);
         FirebaseUser user = firebaseAuth.getCurrentUser();
         textViewUserEmail = (TextView) findViewById(R.id.tv_userEmail);
         textViewUserEmail.setText("Welcome " + user.getEmail());
         buttonLogout = (Button)findViewById(R.id.btn_logout);
 
         buttonLogout.setOnTouchListener(this);
+        buttonSave.setOnTouchListener(this);
 
     }
 
+    private void saveUserInformation(){
+        String name = editTextname.getText().toString().trim();
+        String add = editTextAddress.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(name,add);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child(user.getUid()).setValue(userInformation);
+        Toast.makeText(this, "Information Saved", Toast.LENGTH_SHORT).show();
+
+
+    }
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         switch (motionEvent.getAction()){
@@ -43,6 +70,8 @@ public class ProfileActivity extends AppCompatActivity implements View.OnTouchLi
                 if (view.getId() == R.id.btn_logout){
                     firebaseAuth.signOut();
                     startActivity(new Intent(this,LoginActivity.class));
+                }else if (view.getId() == R.id.btn_AddPeople){
+                    saveUserInformation();
                 }
         }
         return false;
---
+++ app/src/main/java/org/knightdev/firebase101/UserInformation.java
@@ -0,0 +1,17 @@
+package org.knightdev.firebase101;
+
+/**
+ * Created by blood on 2017-03-21.
+ */
+
+public class UserInformation {
+    public String name;
+    public String address;
+
+    public UserInformation( String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+
+}
